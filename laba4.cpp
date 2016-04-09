97e9d60f855b584f211411bee81439c7e5ed0de5
metallurk <romka98@gmail.com>
2016-04-09 11:16:40 +0300

    Update laba4.cpp

--- laba4.cpp
+++ laba4.cpp
@@ -1,6 +1,6 @@
 // laba4.cpp : Defines the entry point for the console application.
-//Вариант 25 Комплексное 
-//Матрицы: умножение, сложение, транспонирование, получение по индексу
+//Р’Р°СЂРёР°РЅС‚ 25 РљРѕРјРїР»РµРєСЃРЅРѕРµ 
+//РњР°С‚СЂРёС†С‹: СѓРјРЅРѕР¶РµРЅРёРµ, СЃР»РѕР¶РµРЅРёРµ, С‚СЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРёРµ, РїРѕР»СѓС‡РµРЅРёРµ РїРѕ РёРЅРґРµРєСЃСѓ
 
 #include "stdafx.h"
 #include "Complex.h"
@@ -8,45 +8,43 @@
 
 int main()
 {
-	
-    Complex value1(5,2);
-    Complex value2(3,-3);
-    cout << "First num:" << value1 << "\n" << "Second num:" << value2 << endl;
-    cout << "Plus: " << value1 + value2 << endl;
-    cout << "Minus: " << value1 - value2 << endl;
-    cout << "Multiplication: " << value1 * value2 << endl;
-    cout << "Division: " << value1 / value2 << endl;    
-    value1 = value2;
-    cout << "Assimilation: " << value1 << " = " << value2 << endl;
+
+	Complex value1(5, 2);
+	Complex value2(3, -3);
+	cout << "First num:" << value1 << "\n" << "Second num:" << value2 << endl;
+	cout << "Plus: " << value1 + value2 << endl;
+	cout << "Minus: " << value1 - value2 << endl;
+	cout << "Multiplication: " << value1 * value2 << endl;
+	value1 = value2;
+	cout << "Assimilation: " << value1 << " = " << value2 << endl;
 
 	cout << "\n\n";
-
+	
 	Matrixx matr1(2, 3);
-	matr1.data[0]=1;
-	matr1.data[1]=3;
-	matr1.data[2]=2;
-	matr1.data[3]=0;
-	matr1.data[4]=4;
-	matr1.data[5]=-1;
+	for (int i = 0; i < 6; i++)
+	{
+		Complex temp(rand() % 20, rand() % 10);
+		matr1.data[i] = temp;
+	}
 	Matrixx matr2(3, 4);
-	matr2.data[0]=2;
-	matr2.data[1]=0;
-	matr2.data[2]=-1;
-	matr2.data[3]=1;
-	matr2.data[4]=3;
-	matr2.data[5]=-2;
-	matr2.data[6]=1;
-	matr2.data[7]=2;
-	matr2.data[8]=0;
-	matr2.data[9]=1;
-	matr2.data[10]=2;
-	matr2.data[11]=3;
-	Matrixx::printmat(matr1);
-	cout<<"\n";
-	Matrixx::printmat(matr2);
-	//Matrixx::printmat(matr1+matr2);
+	for (int i = 0; i < 12; i++)
+	{
+		Complex temp(rand() % 20, rand() % 10);
+		matr2.data[i] = temp;
+	}
 	
-	
+	Matrixx matr3(2, 3);
+	for (int i = 0; i < 6; i++)
+	{
+		Complex temp(rand() % 20, rand() % 10);
+		matr3.data[i] = temp;
+	}
+	cout << "matr1: \n" << matr1 << "matr2: \n" << matr2 << "matr3: \n" << matr3 << endl;
+	cout << "matr1+matr3: \n" << (matr1+matr3) << endl;
+	cout << "matr1*matr2: \n" << matr1*matr2 << endl;
+	//cout << ~matr1 << endl;
+	cout << "matr1: \n" << matr1 << "matr2: \n" << matr2 << "matr3: \n" << matr3 << endl;
+	cout << "matr1(1,1) = " << Matrindex(matr1, 1, 1) << endl;
 	system("pause");
-    return 0;
+	return 0;
 }
