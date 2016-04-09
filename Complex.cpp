45b7a21be0e28a50d94ae7a58ca3325ebfd1bf81
metallurk <romka98@gmail.com>
2016-04-09 11:10:10 +0300

    Update Complex.cpp
    Исправил формулу умножения комплексных чисел

--- Complex.cpp
+++ Complex.cpp
@@ -7,88 +7,79 @@ Complex::Complex(void)
 	re = 0;
 	im = 0;
 }
- Complex::Complex (double r) // конструктор по умолчанию
-   {
-      re = r; 
-      im = 0;
-   }
- 
- Complex::Complex (double r, double i) // конструктор по умолчанию
-   {
-      re = r; 
-      im = i;
-   } 
- 
- Complex::Complex (Complex &c) // конструктор копирования
-   {
-      re = c.re; 
-      im = c.im;
-   } 
+Complex::Complex(double r) // РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+{
+	re = r;
+	im = 0;
+}
+
+Complex::Complex(double r, double i) // РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+{
+	re = r;
+	im = i;
+}
+
+Complex::Complex(Complex &c) // РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РєРѕРїРёСЂРѕРІР°РЅРёСЏ
+{
+	re = c.re;
+	im = c.im;
+}
 
 Complex::~Complex(void)
 {
 }
 
-  Complex & Complex::operator = (Complex &c)
-   {
-      re = c.re;
-      im = c.im;
- 
-      return (*this);
-   }
- 
-   Complex Complex::operator + (Complex &c)
-   {
-      Complex temp;
- 
-      temp.re = re + c.re;
-      temp.im = im + c.re;
- 
-      return temp;
-   }
- 
-   Complex Complex::operator - (Complex &c)
-   {
-      Complex temp;
- 
-      temp.re = re - c.re;
-      temp.im = im - c.re;
- 
-      return temp;
-   }
- 
-   Complex Complex::operator * (Complex &c)
-   {
-      Complex temp;
- 
-      temp.re = re*c.re;
-      temp.im = re*c.im;
- 
-      return temp;
-   }
- 
-   Complex Complex::operator / (Complex &c) 
-   {
-      Complex temp;
- 
-      double r = c.re * c.re + c.im * c.im;
-      temp.re = (re * c.re + im * c.im) / r;
-      temp.re = (im * c.re - re * c.im) / r;
- 
-      return temp;
-   } 
- 
+Complex & Complex::operator = (Complex &c)
+{
+	re = c.re;
+	im = c.im;
+
+	return (*this);
+}
+
+Complex Complex::operator + (Complex &c)
+{
+	Complex temp;
+
+	temp.re = re + c.re;
+	temp.im = im + c.im;
+
+	return temp;
+}
+
+Complex Complex::operator - (Complex &c)
+{
+	Complex temp;
+
+	temp.re = re - c.re;
+	temp.im = im - c.re;
+
+	return temp;
+}
+
+Complex Complex::operator * (Complex &c)
+{
+	Complex temp;
+
+	temp.re = re*c.re-im*c.im;
+	temp.im = re*c.im+im*c.re;
+
+	return temp;
+}
+
+
+
 ostream &operator<<(ostream &out, Complex &c)
 {
-   out << "(" << c.re << ") + I (" << c.im << ")";
- 
-   return out;
+	out << "(" << c.re << ") + I (" << c.im << ")";
+
+	return out;
 }
- 
+
 istream &operator>>(istream &in, Complex &c)
 {
-   in >> c.re >> c.im;
- 
-   return in;
+	in >> c.re >> c.im;
+
+	return in;
 }
- 
+
